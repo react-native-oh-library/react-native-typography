3f9775aa4934cc2d3748c7d42fe17c35dd586dc7
kongchuiyu <kongchuiyu@h-partners.com>
2024-12-16 11:48:50 +0800

    chore: add license header and format code style

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
+/*
+ * Copyright (c) 2024 Huawei Device Co., Ltd. All rights reserved
+ * Use of this source code is governed by a MIT license that can be
+ * found in the LICENSE file.
+ */
+
 export { human } from "react-native-typography";
 export { humanDense } from "react-native-typography";
 export { humanTall } from "react-native-typography";
